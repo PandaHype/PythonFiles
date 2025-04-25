3c84ce0c15c8f94e9d01b5ee1386ea36b6ac049f
PandaHype <marcus.idestam-almquist@outlook.com>
2025-04-25 11:38:06 +0200

    fixed sideways collision, added jump on death anim, fixed up anims and facing directions.

--- platformer.py
+++ platformer.py
@@ -1,6 +1,4 @@
 import pygame
-import time
-import os
 from sys import exit
 
 pygame.init()
@@ -22,8 +20,8 @@ ground_rect = ground_surf.get_rect()
 ground_rect = pygame.Rect(0, 700, SCREEN_WIDTH, 200)
 
 # Object Rects
-player_rect = pygame.Rect(200, 600, 50, 60)
-platform1_rect = pygame.Rect(750, 500, 100, 20)
+player_rect = pygame.Rect(200, 600, 55, 64)
+platform1_rect = pygame.Rect(750, 680, 100, 20)
 platform2_rect = pygame.Rect(1200, 600, 100, 20)
 platform3_rect = pygame.Rect(1500, 400, 100, 20)
 
@@ -34,19 +32,24 @@ player_sprite = [pygame.image.load('Sprites/player/sprite_1.png').convert_alpha(
 
 frame_index = 0
 frame_timer = 0
-animation_speed = 30
+animation_speed = 20
 
 
 player_vel = 0
 bg_scroll = 0
 scroll_speed = 5
-collision = False 
+left_collision = False
+right_collision = False
+death = False
+death_jump = False
+
+# Store player facing directon [1 = Left, 0 = Right]
+prev_direction = 0
 
 platforms = [ground_rect, platform1_rect, platform2_rect, platform3_rect]
 platforms_draw = [platform1_rect, platform2_rect, platform3_rect]
 
 landed = 0
-
 
 
 while True:
@@ -71,17 +74,17 @@ while True:
     keys = pygame.key.get_pressed()
 
     # Handle horizontal movement
-    if keys[pygame.K_a] and not collision:
+    if keys[pygame.K_a] and not right_collision and not death:
         bg_scroll += scroll_speed
         for platform in platforms:
             platform.x += scroll_speed
-    if keys[pygame.K_d] and not collision:
+    if keys[pygame.K_d] and not left_collision and not death:
         bg_scroll -= scroll_speed
         for platform in platforms:
             platform.x -= scroll_speed
 
-    # Move ground to prevent falling off
-    ground_rect.x = player_rect.x
+    # Move ground to prevent falling off, [DEBUG] DISABLE FOR ACTUAL GAME!
+    # ground_rect.x = player_rect.x
 
     if frame_timer >= animation_speed:
         frame_index = (frame_index + 1) % len(player_sprite)
@@ -92,9 +95,24 @@ while True:
     
     current_image = player_sprite[frame_index]
     if keys[pygame.K_d]:
-        screen.blit(current_image,(190,630))
-    else:
-        screen.blit(player_sprite[0],(190,630))
+        screen.blit(current_image,player_rect.topleft)
+        prev_direction = 0
+    elif keys[pygame.K_d]:
+        screen.blit(current_image,player_rect.topleft)
+        prev_direction = 0
+    if keys[pygame.K_a]:
+        current_image = pygame.transform.flip(current_image,flip_x=True,flip_y=False)
+        screen.blit(current_image,player_rect.topleft)
+        prev_direction = 1
+    elif keys[pygame.K_a]:
+        current_image = pygame.transform.flip(current_image,flip_x=True,flip_y=False)
+        screen.blit(current_image,player_rect.topleft)
+        prev_direction = 1
+    if prev_direction == 0 and not keys[pygame.K_d]:
+        screen.blit(player_sprite[0],player_rect.topleft)
+    elif prev_direction == 1 and not keys[pygame.K_a]:
+        current_image = pygame.transform.flip(player_sprite[0],flip_x=True,flip_y=False)
+        screen.blit(current_image,player_rect.topleft)
 
     # Save previous values before moving
     prev_bottom = player_rect.bottom
@@ -121,16 +139,17 @@ while True:
                 player_vel = 0
 
     # Check for side collisions (left and right) to prevent scrolling
-    collision = False
+    left_collision = False
+    right_collision = False
     for platform in platforms:
         if player_rect.colliderect(platform):
             if prev_right > platform.left and prev_left < platform.left:
                 # Collided from the left
-                collision = True
+                left_collision = True
                 break
             if prev_left < platform.right and prev_right > platform.right:
                 # Collided from the right
-                collision = True
+                right_collision = True
                 break
 
     # Apply gravity if not on a platform
@@ -140,8 +159,13 @@ while True:
         if player_vel < 15:
             player_vel += 0.75
         landed = 0
-
     frame_timer += 1
+
+    if player_rect.bottom >= 900:
+        death = True
+    if death == True and not death_jump:
+        player_vel = -25
+        death_jump = True
 
 
     pygame.display.update()
